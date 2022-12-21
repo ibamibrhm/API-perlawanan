af09624d7d3e726b3fb18ac554148f08dab49246
Ibrahim <ibrahimibam29@gmail.com>
2022-12-21 08:30:55 +0700

    update project library

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 require('dotenv').config();
+const fastify = require('fastify')({
+  logger: true,
+});
+const cors = require('@fastify/cors');
+
 const port = process.env.PORT || 3000;
-const app = require('fastify')();
-app.register(require('fastify-cors'));
-app.register(require('fastify-mongodb'), {
+
+fastify.register(cors);
+fastify.register(require('@fastify/mongodb'), {
   forceClose: true,
-  url: process.env.DB_CONNECTION
+  url: process.env.DB_CONNECTION,
 });
 
-app.get('/', function (_, reply) {
-  const db = this.mongo.db;
-  db.collection('quotes', onCollection);
-
-  async function onCollection(err, col) {
-    if (err) return reply.send(err);
-
-    const [quote] = await col.aggregate([{ $project: { author: 1, quote: 1, _id: 0 } }, { $sample: { size: 1 } }]).toArray();
-    reply.send(quote);
+fastify.get('/', async function (req, reply) {
+  try {
+    const quotes = this.mongo.db.collection('quotes');
+    const [quote] = await quotes.aggregate([{ $project: { author: 1, quote: 1, _id: 0 } }, { $sample: { size: 1 } }]).toArray();
+    return reply.send(quote);      
+  } catch (error) {
+    return reply.send(error)
   }
 });
 
-app.listen(port, '0.0.0.0', () => console.log('app running on port:', port));
+fastify.listen({ port, host: '0.0.0.0' }, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  console.log(`Server is now listening on ${address}`)
+})
 
-module.exports = app;
+module.exports = fastify;
